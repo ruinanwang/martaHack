50d03dadf285d2ffd36ead912acdff984935c93b
JingboWang1997 <wjbjimmy@gmail.com>
2017-09-09 19:05:14 -0400

    update feature and location name

--- Layout.java
+++ Layout.java
@@ -9,6 +9,9 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.layout.StackPane;
+import javafx.geometry.Pos;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -19,6 +22,7 @@ public class Layout extends Application {
 	private static Button update;
 	private static Stage stage;
 	private static GridPane grid;
+    private static StackPane base;
     private static Button button;
 
     private static Reader reader;
@@ -34,30 +38,40 @@ public class Layout extends Application {
         stage = primaryStage;
         window = new BorderPane();
         button = new Button("update");
+        button.setOnMouseClicked(e -> {
+            visualize();
+        });
 
         grid = new GridPane();
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
                 Rectangle current = new Rectangle(10, 10);
-                current.setFill(Color.DARKGRAY);
                 grid.add(current, i, j);
             }
         }
 
-        for (Location location: reader.readInputText()) {
-            int x = location.getX();
-            int y = location. getY();
-            Rectangle r = new Rectangle(10, 10);
-            double weight = (double)location.getpFlow() / (double)100;
-            System.out.println(weight);
-            double red = (double)255 * weight;
-            r.setFill(Color.rgb((int)red, 0, 0));
-            grid.add(r, x, y);
-        }
+        visualize();
+        base = new StackPane(grid);
 
         window.setCenter(grid);
         window.setRight(button);
+        window.setLeft(new Text("Location"));
         stage.setScene(new Scene(window));
         stage.show();
+    }
+
+    public void visualize() {
+        for (Location location: reader.readInputText()) {
+                int x = location.getX();
+                int y = location. getY();
+                Rectangle r = new Rectangle(10, 10);
+                r.setOnMouseClicked(e -> {
+                    window.setLeft(new Text(location.getName()));
+                });
+                double weight = (double)location.getpFlow() / (double)100;
+                double red = (double)255 * weight;
+                r.setFill(Color.rgb((int)red, 70, 70));
+                grid.add(r, x, y);
+            }
     }
 }
